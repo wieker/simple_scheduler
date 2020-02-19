e4f5958ffa03060c390e48e21dfd52a35c35a873
kabramovich <wieker@gmail.com>
2020-02-20 01:03:20 +0200

    Started some test

--- src/test/java/org/allesoft/simple_scheduler/SchedulerTest.java
+++ src/test/java/org/allesoft/simple_scheduler/SchedulerTest.java
@@ -1,4 +1,18 @@
 package org.allesoft.simple_scheduler;
 
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class SchedulerTest {
+    @InjectMocks
+    Scheduler scheduler;
+
+    @Test
+    public void checkAllocation() {
+
+    }
+
 }
