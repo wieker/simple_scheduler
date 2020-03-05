1d32f3a170de21867d228e8969b944f295a7768c
kabramovich <wieker@gmail.com>
2020-03-05 20:51:12 +0200

    Google API key var

--- src/main/java/org/allesoft/simple_scheduler/scheduler/GeoMatrix.java
+++ src/main/java/org/allesoft/simple_scheduler/scheduler/GeoMatrix.java
@@ -9,12 +9,13 @@ import com.google.maps.model.TravelMode;
 public class GeoMatrix {
     public static void main(String[] args) throws Exception {
         GeoApiContext apiContext = new GeoApiContext.Builder()
-                .apiKey("")
+                .apiKey(System.getProperty("google.matrix.api.key"))
                 .build();
 
         DistanceMatrixApiRequest req= DistanceMatrixApi.newRequest(apiContext);
         DistanceMatrix t=req.origins("Estonia").destinations("London")
                 .mode(TravelMode.DRIVING).await();
         System.out.println(t.rows[0].elements[0].distance);
+        apiContext.shutdown();
     }
 }
