e56ad4e17bbf9771c020a5d5b303a47e8ea6482c
kabramovich <wieker@gmail.com>
2020-03-08 19:58:35 +0200

    Elementary ThreadLocal Array Blocking Queue without notFull condition

--- src/main/java/org/allesoft/simple_scheduler/nrf52/mesh/Emulate.java
+++ src/main/java/org/allesoft/simple_scheduler/nrf52/mesh/Emulate.java
@@ -4,10 +4,36 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Emulate implements Runnable {
+    public static ThreadLocalArrayBackedQueue<Integer> queue = new ThreadLocalArrayBackedQueue<>();
+
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 10; i ++) {
-            executorService.submit(new Emulate());
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i ++) {
+                    try {
+                        queue.put(i);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        for (int i = 0; i < 3; i ++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        for (;;) {
+                            System.out.println(queue.take());
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
     }
 
