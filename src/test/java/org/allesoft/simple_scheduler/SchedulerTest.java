1f9a39f0ea1906650cae583f25ae1b1e69f33dc9
kabramovich <wieker@gmail.com>
2020-03-14 14:40:12 +0200

    simple test

--- src/test/java/org/allesoft/simple_scheduler/SchedulerTest.java
+++ src/test/java/org/allesoft/simple_scheduler/SchedulerTest.java
@@ -1,20 +1,35 @@
 package org.allesoft.simple_scheduler;
 
+import org.allesoft.simple_scheduler.scheduler.Prefetcher;
 import org.allesoft.simple_scheduler.scheduler.Scheduler;
+import org.allesoft.simple_scheduler.scheduler.SchedulerImpl;
 import org.allesoft.simple_scheduler.scheduler.service.RoutingService;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class SchedulerTest {
     @InjectMocks
-    Scheduler scheduler;
+    Scheduler scheduler = new SchedulerImpl(null, null, null, null, null, null, null);
+    @Mock
     AlgorithmService algorithmService;
-    RoutingService routingService;
+    @Mock
+    RoutingService simplestRoutingService;
+    @Mock
+    RoutingService lowCostRoutingService;
+    @Mock
+    RoutingService finalRoutingService;
+    @Mock
     TaskExecutorService taskExecutorService;
+    @Mock
     OptionCalculationCache optionCalculationCache;
+    @Mock
+    Snapshot snapshot;
+    @Mock
+    Prefetcher prefetcher;
 
     @Test
     public void checkAllocation() {
