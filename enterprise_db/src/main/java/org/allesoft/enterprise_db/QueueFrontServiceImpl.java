71eccba2e8ffd31d902c50185923f11e3d9122cb
kabramovich <wieker@gmail.com>
2020-03-29 10:34:50 +0300

    changed to prototype bean

--- enterprise_db/src/main/java/org/allesoft/enterprise_db/QueueFrontServiceImpl.java
+++ enterprise_db/src/main/java/org/allesoft/enterprise_db/QueueFrontServiceImpl.java
@@ -1,20 +1,29 @@
 package org.allesoft.enterprise_db;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.task.TaskExecutor;
 
-import java.util.List;
-
-public class QueueFrontServiceImpl implements QueueFrontService {
-    private List<Runnable> fillers;
+public class QueueFrontServiceImpl implements QueueFrontService, ApplicationContextAware {
     private TaskExecutor taskExecutor;
+    private ApplicationContext applicationContext;
+    private Integer fillersCount;
 
-    public QueueFrontServiceImpl(List<Runnable> fillers, TaskExecutor taskExecutor) {
-        this.fillers = fillers;
+    public QueueFrontServiceImpl(TaskExecutor taskExecutor, Integer fillersCount) {
         this.taskExecutor = taskExecutor;
+        this.fillersCount = fillersCount;
     }
 
     @Override
     public void fill() {
-        fillers.forEach(taskExecutor::execute);
+        for (int i = 0; i < fillersCount; i ++) {
+            taskExecutor.execute((Runnable) applicationContext.getBean("filler"));
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
