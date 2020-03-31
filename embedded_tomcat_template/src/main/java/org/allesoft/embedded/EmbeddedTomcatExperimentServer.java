939f81c150b744ba73d4bd8d84d3d8c0464b4303
Kirill Abramovich <wieker-@-gmail.com>
2020-03-31 17:42:37 +0300

    working WebMVC

--- embedded_tomcat_template/src/main/java/org/allesoft/embedded/EmbeddedTomcatExperimentServer.java
+++ embedded_tomcat_template/src/main/java/org/allesoft/embedded/EmbeddedTomcatExperimentServer.java
@@ -4,7 +4,8 @@ import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.File;
@@ -19,7 +20,7 @@ public class EmbeddedTomcatExperimentServer {
 
         Context ctx = tomcat.addContext("", new File("d2").getAbsolutePath());
 
-        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        ConfigurableWebApplicationContext webApplicationContext = new GenericWebApplicationContext();
         webApplicationContext.setParent(applicationContext);
         Tomcat.addServlet(ctx, "spring", new DispatcherServlet(webApplicationContext));
         ctx.addServletMappingDecoded("/", "spring");
